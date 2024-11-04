ff0179a39d7843497fc7c95c58a5049858085aa9
Sandeep Kumar <144022647+SandeepKumar1947@users.noreply.github.com>
2024-11-03 19:17:10 -0500

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,26 +1,22 @@
 import java.util.Scanner;
 
 public class Fibonacci {
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in); 
-        System.out.print("Enter an integer n to find the nth Fibonacci number: ");
-        int n = scanner.nextInt(); 
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
 
         int result = fibonacci(n);
-        System.out.println("Fibonacci of " + n + " is: " + result);
-        
-        scanner.close(); 
-    }
-
-    
-    public static int fibonacci(int n) {
-        if (n <= 0) {
-            return 0; 
-        } else if (n == 1) {
-            return 1; 
-        } else {
-            return fibonacci(n - 1) + fibonacci(n - 2); 
-        }
+        System.out.println("The " + n + " Fibonacci number is: " + result);
+        sc.close();
     }
 }
+
